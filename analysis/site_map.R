c31e74ef0a0ac758ddfa25059bcac0c11ac147de
johngodlee <johngodlee@gmail.com>
2018-03-13 16:12:32 +0000

    Added shapefiles, updated map but still broken

--- analysis/site_map.R
+++ analysis/site_map.R
@@ -26,38 +26,43 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 seedlings <- read.csv("data/seedlings.csv")
 
 # Manu shapefile
-manu <- readOGR("data/manu_outline", "manu")
-# plot(manu, axes = T)
-proj4string(manu)
+manu_wdpa <- readOGR("data/manu_wdpa_outline", "manu_wdpa")
+proj4string(manu_wdpa)
 
 # Peru shapefile
 peru <- readOGR("data/peru_outline", "peru")
 # plot(peru, axes = T)
 proj4string(peru)
 
-# ggmap of manu outline
-bb_manu <- bbox(manu)
-manu_ggmap <- ggmap(get_map(location = bb_manu, zoom = 9))
-manu_ggmap
+# Elevation data for Manu in raster format from USGS
+elevation_1 <- raster("data/usgs_dem/ASTGTM2_S13W071_dem.tif")
+elevation_2 <- raster("data/usgs_dem/ASTGTM2_S13W072_dem.tif")
+elevation_3 <- raster("data/usgs_dem/ASTGTM2_S13W073_dem.tif")
+elevation_4 <- raster("data/usgs_dem/ASTGTM2_S14W071_dem.tif")
+elevation_5 <- raster("data/usgs_dem/ASTGTM2_S14W072_dem.tif")
+elevation_6 <- raster("data/usgs_dem/ASTGTM2_S14W073_dem.tif")
+elevation_all <- merge(elevation_1, elevation_2, elevation_3, elevation_4, elevation_5, elevation_6)
 
-# Map of Peru with Elevation data using getData() from {raster}
-elevation <- getData("alt", country = "Peru")
-# convert raster to points for plotting
-elev_p <- rasterToPoints(elevation)
+# Elevation data for Peru using getData() from {raster}
+elevation_peru <- getData("alt", country = "Peru")
 
-# Make the points a dataframe for ggplot
-elev_df <- data.frame(elev_p)
+# Peru rivers lines
+water <- readOGR("data/peru_water", "sa_riv_15s")
 
-# Make appropriate column headings
-colnames(elev_df) <- c("lon", "lat", "elev")
+# Create objects for map of Peru with manu outline ----
+# convert peru raster to points for plotting
+elev_peru_p <- rasterToPoints(elevation_peru)
+elev_peru_df <- data.frame(elev_peru_p)
+colnames(elev_peru_df) <- c("lon", "lat", "elev")
 
-manu@data$id <- rownames(manu@data)
-manu_p = fortify(manu, region="id")
-manu_df = join(manu_p, manu@data, by="id")
+# Convert manu polygon to df for plotting
+manu_wdpa@data$id <- rownames(manu_wdpa@data)
+manu_p = fortify(manu_wdpa, region="id")
+manu_df = join(manu_p, manu_wdpa@data, by="id")
 
+# Plot of peru with manu outline on top ----
 
-# Plot with manu outline on top
-ggplot(data = elev_df, aes(x = lon, y = lat)) +
+peru_map_ggplot <- ggplot(data = elev_peru_df, aes(x = lon, y = lat)) +
 	geom_raster(aes(fill=elev)) + 
 	geom_polygon(data = manu_df, aes(x = long, y = lat, group = group), 
 							 fill = "grey", colour = "white", alpha = 0.5) + 
@@ -66,19 +71,81 @@ ggplot(data = elev_df, aes(x = lon, y = lat)) +
 	theme_classic() + 
 	guides(fill = guide_legend(title="Elevation (m)"))
 
-# Find out the position of each plot from mean of seedling locations ----
+
+
+# Create objects for map of study site ----
+# Find out the position of each plot from mean of seedling locations
 site_loc <- seedlings %>%
 	group_by(Site) %>%
-	summarise(lat_mean = mean(Lat.DD),
-						lon_mean = mean(Long.DD),
-						elev = mean(Elevation))
+	dplyr::summarise(lat_mean = mean(Lat.DD),
+									 lon_mean = mean(Long.DD),
+									 elev = mean(Elevation))
+
+# Create bounding box for map based on extent of plot locations
+site_bbox <- as(raster::extent(-71.75, -71,25, -13.25, -12.5), "SpatialPolygons")
+
+# Crop elevation dem to site locations
+elevation_clipped <- crop(elevation_all, extent(site_bbox))
+elevation_clipped <- mask(elevation_clipped, site_bbox)
+
+# Convert cropped elevation dem to data frame for ggplot
+elev_spdf <- as(elevation_clipped, "SpatialPixelsDataFrame")
+elev_df <- as.data.frame(elev_spdf)
+colnames(elev_df) <- c("elev", "lon", "lat")
+
+# Crop water to site locations
+# water_clipped <- crop(water, raster::extent(site_bbox))
+# Write to object
+# writeSpatialShape(x = water_clipped, fn = "water_clipped")
+
+# Convert cropped water lines to data frame for ggplot
+water_sldf <- as(water, "SpatialLinesDataFrame")
+water_clipped <- raster::intersect(water_sldf, site_bbox)
+water_df <- fortify(water_clipped)
 
 
-# Maps of study sites, zoomed in on Manu
+# Crop Manu outline to site_loc bbox
+manu_wdpa_clipped <- crop(manu_wdpa, extent(site_bbox))
+# Convert manu polygon to df for plotting
+manu_clipped_p = fortify(manu_wdpa_clipped, region="id")
+manu_clipped_df = join(manu_clipped_p, manu_wdpa@data, by="id")
 
 
+# ggmap of study sites, zoomed in on Manu ----
 
-#Plot maps
+site_map_ggplot <- ggplot() +
+	geom_raster(data = elev_df, aes(x = lon, y = lat, fill=elev)) +  # DEM
+	geom_polygon(data = manu_clipped_df, aes(x = long, y = lat, group = group), 
+							 fill = "grey", colour = "white", alpha = 0.3) + 
+	geom_point(data = site_loc, aes(x = lon_mean, y = lat_mean), 
+						 size = 5, alpha = 0.8, fill = "#D64E0F", colour = "black", pch=21) +  # Site locations
+	#geom_path(data = water_df, aes(x = long, y = lat, group=group), colour = "blue") +  # River locations
+	scale_fill_viridis() +  # Colour for dem
+	theme_classic() +
+	xlab(expression(paste("Longitude (",degree,")"))) + 
+	ylab(expression(paste("Latitude (",degree,")")))
+ggsave(file = "map_test.pdf", plot = site_map_ggplot)
+
+# Would be good to have river on the map
+# Map is all off kilter now, fuck it
+
+# Save all plots ----
+
+# Create list of plots using grep
+plot_pattern<-grep("_ggplot", names(.GlobalEnv), value=TRUE)
+ggplot_list<-do.call("list", mget(plot_pattern))
+
+# Compile
+mapply(ggsave, 
+			 file = paste0("img/", names(ggplot_list), ".pdf"), 
+			 plot = ggplot_list, 
+			 width = 30, height = 25, units = "cm")
+
+############################################
+######### OLD MAPS #########################
+############################################
+
+# Plot maps
 myLocation <- c(min(camp_loc$Site_long_mean.Long.DD_mean), min(camp_loc$Lat.DD_mean), max(camp_loc$Site_long_mean.Long.DD_mean), max(camp_loc$Lat.DD_mean))
 Manu_gg <- get_map(location = myLocation, zoom = 10, maptype = "terrain", )
 ggmap(Manu_gg) + scale_shape_identity() + 
